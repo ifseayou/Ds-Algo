8a92329847fbff6d978a23d2953fd6603beb12f2
ifseayou <ifseayou@gmail.com>
2019-04-09 09:21:24 +0800

    update

--- greddy/IPO.java
+++ greddy/IPO.java
@@ -9,41 +9,42 @@ import java.util.PriorityQueue;
  * w 为启动资金，每次只能做一个项目，如果想要做项目的话，当前手里的启动资金要大于项目的花费
  * k 最多做k个项目
  * 最终获取的最大收益（也就是最终的启动资金）
- *
+ * <p>
  * 解决：
  * 所有的项目，按照cost的维度，生成小根堆，然后只要cost小于w的，弹出，按照profit的维度放置到大根堆中
  * 小根堆里面是所有的项目，大根堆里面是解锁的项目，然后在开始弹出（做）大根堆里的项目，此时w增加，在去判断
  * 小根堆里还可以解锁哪些项目，一直做K个结束，或者是做到最后不能做了（大根堆空了）
- *
  */
 public class IPO {
     public static class Program {
         private int c;
         private int p;
-        public Program(int c , int p){
+
+        public Program(int c, int p) {
             this.c = c;
             this.p = p;
         }
     }
-    public static class MinCostComparator implements Comparator<Program>{
+
+    public static class MinCostComparator implements Comparator<Program> {
         @Override
         public int compare(Program o1, Program o2) {
             return o1.c - o2.c;
         }
     }
 
-    public static class MaxProfitComparator implements  Comparator<Program>{
+    public static class MaxProfitComparator implements Comparator<Program> {
         @Override
         public int compare(Program o1, Program o2) {
             return o2.p - o1.p;
         }
     }
 
-    public static int maxProfit(int w,int k,int[] costs,int[] profits){
+    public static int maxProfit(int w, int k, int[] costs, int[] profits) {
         // 将数组封装为项目
         Program[] programs = new Program[costs.length];
         for (int i = 0; i < costs.length; i++) {
-            programs[i] = new Program(costs[i],profits[i]);
+            programs[i] = new Program(costs[i], profits[i]);
         }
 
         PriorityQueue<Program> minCostHeap = new PriorityQueue<>(new MinCostComparator());
@@ -57,20 +58,21 @@ public class IPO {
         // 开始做项目，
         for (int i = 0; i < k; i++) {
             // 解锁项目
-            while(!minCostHeap.isEmpty() && minCostHeap.peek().c < w){
+            while (!minCostHeap.isEmpty() && minCostHeap.peek().c < w) {
                 maxProfitHeap.add(minCostHeap.poll());
             }
             // 如果大根堆没有了项目，说明无法解锁新的项目了
-            if (maxProfitHeap.isEmpty()){
+            if (maxProfitHeap.isEmpty()) {
                 return w;
             }
             w += maxProfitHeap.poll().p;
         }
         return w;
     }
+
     public static void main(String[] args) {
-        int[] costs = {10,20,30};
-        int[] profits = {3,4,5};
+        int[] costs = {10, 20, 30};
+        int[] profits = {3, 4, 5};
         System.out.println(maxProfit(17, 3, costs, profits));
     }
 }
---
+++ trie_set/LandsNums.java
@@ -0,0 +1,49 @@
+package com.isea.dw.trie_set;
+
+/**
+ * 现在有一个矩阵，矩阵中只有元素0 和元素1 ，连在一片的1 称之为一个岛，求一个矩阵中的到的个数;
+ *
+ * 如果是一个巨大的矩阵，需要将该矩阵做一个map分解，分解之后计算岛的数量，然后在做reduce求出一共岛的数量
+ * 此时需要收集分岛的边界信息，在做reduce的时候，根据边界信息是否属于同样的一个代表节点，来判断有多少个岛
+ */
+public class LandsNums {
+    public static int lansNums(int[][] matrix){
+        if (matrix == null || matrix.length == 0){
+            return 0;
+        }
+        int res = 0;
+        int M = matrix.length;
+        int N = matrix[0].length;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (matrix[i][j] == 1){
+                    res ++;
+                    infect(matrix,i,j,M,N);
+                }
+            }
+        }
+        return res;
+    }
+
+    // 感染函数，如果遇到了1，就将其设置为2，并将和其连在一起的1都修改为2
+    private static void infect(int[][] matrix, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || matrix[i][j] != 1){ // 这里要注意matrix[i][j] =0或者2的时候，需要stop感染
+            return;
+        }
+        matrix[i][j] = 2;
+        infect(matrix,i + 1,j,m,n);
+        infect(matrix,i - 1,j,m,n);
+        infect(matrix,i,j + 1,m,n);
+        infect(matrix,i,j - 1,m,n);
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 1, 1, 0},
+                {1, 1, 0, 0, 0, 1, 1, 1, 0}
+        };
+        System.out.println(lansNums(m));// 2
+    }
+
+}
